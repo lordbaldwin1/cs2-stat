c7635eed1b2ddd1aad9db357ac79fb3adbe53bd7
zachar7 <springerzman@gmail.com>
2025-07-18 01:29:52 -0700

    scrape leetify concurrently for matches, working on scraping match data concurrently

--- go.mod
+++ go.mod
@@ -5,3 +5,14 @@ go 1.24.4
 require github.com/joho/godotenv v1.5.1
 
 require github.com/mattn/go-sqlite3 v1.14.28
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20250715215929-4738bcb231c7 // indirect
+	github.com/chromedp/chromedp v0.13.7 // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/go-json-experiment/json v0.0.0-20250714165856-be8212f5270d // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+)
--- internal/server/fetch_scrape.go
+++ internal/server/fetch_scrape.go
@@ -9,7 +9,10 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
+
+	"github.com/chromedp/chromedp"
 )
 
 type Players struct {
@@ -36,6 +39,7 @@ type PlayerDetails struct {
 
 const topPlayersURL string = "https://open.faceit.com/data/v4/rankings/games/cs2/regions/"
 const playerDetailsURL string = "https://open.faceit.com/data/v4/players/"
+const leetifyUserURL string = "https://leetify.com/app/profile/"
 
 func (s *Server) FetchAndScrape() error {
 	log.Println("Starting fetching and scraping...")
@@ -72,16 +76,154 @@ func (s *Server) FetchAndScrape() error {
 		faceitURL := strings.ReplaceAll(player.FaceitURL, "{lang}", "en")
 		log.Println("Adding player to db: ", player.Nickname)
 		log.Println(faceitURL)
-		s.db.CreatePlayer(ctx, database.CreatePlayerParams{
+		addedPlayer, err := s.db.CreatePlayer(ctx, database.CreatePlayerParams{
 			SteamID:   player.SteamID64,
 			Name:      player.Nickname,
 			Country:   player.Country,
 			FaceitUrl: faceitURL,
 			Avatar:    player.Avatar,
 		})
+		if err != nil {
+			return fmt.Errorf("error: %s", err)
+		}
+		log.Printf("Added player: %s to db\n", addedPlayer.Name)
 	}
 
+	var leetifyURLs []string
+	for _, playerDetail := range playerDetails {
+		url := leetifyUserURL + playerDetail.SteamID64
+		leetifyURLs = append(leetifyURLs, url)
+	}
+
+	// concurrent scraping
+	err = s.scrapeLeetifyWithWorkers(leetifyURLs)
+	if err != nil {
+		return err
+	}
+
+	// old scraping
+	// err = s.scrapeLeetify(playerDetails)
+	// if err != nil {
+	// 	return err
+	// }
+
 	return nil
+}
+
+func (s *Server) scrapeLeetify(players []PlayerDetails) error {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	ctx, cancel = context.WithTimeout(ctx, 120*time.Second)
+	defer cancel()
+
+	log.Println("Gathering match links from Leetify")
+	var matchLinks []string
+	for _, player := range players {
+		url := leetifyUserURL + player.SteamID64
+
+		var playerLinks []string
+		err := chromedp.Run(ctx,
+			chromedp.Navigate(url),
+			chromedp.Sleep(10*time.Second),
+			chromedp.Evaluate(`
+				Array.from(document.querySelectorAll('a.ng-star-inserted[href^="/app/match-details/"]'))
+					.slice(0, 5)
+					.map(a => a.href)
+			`, &playerLinks),
+		)
+		if err != nil {
+			return fmt.Errorf("error: leetify user page: %s", err)
+		}
+		matchLinks = append(matchLinks, playerLinks...)
+		log.Println("gathered: ", len(matchLinks), " matches so far")
+	}
+
+	log.Println("finished gathering matches, scraping matches")
+	var tableData [][]string
+	for _, matchURL := range matchLinks {
+		err := chromedp.Run(ctx,
+			chromedp.Navigate(matchURL),
+			chromedp.WaitVisible(`table`, chromedp.ByQuery),
+			chromedp.Evaluate(`
+				Array.from(document.querySelectorAll('table tr'))
+					.map(row => Array.from(row.querySelectorAll('td'))
+					.map(cell => cell.textContent.trim())
+			)
+			`, &tableData),
+		)
+		if err != nil {
+			return fmt.Errorf("error: leetify match page: %s", err)
+		}
+		for _, row := range tableData {
+			log.Println(row)
+		}
+	}
+	return nil
+}
+
+func (s *Server) scrapeLeetifyWithWorkers(playerURLs []string) error {
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), chromedp.DefaultExecAllocatorOptions[:]...)
+	defer cancel()
+	parentCtx, cancel := chromedp.NewContext(allocCtx)
+	defer cancel()
+
+	numWorkers := 5
+	jobs := make(chan string, len(playerURLs))
+	results := make(chan []string, len(playerURLs))
+
+	var wg sync.WaitGroup
+	for range numWorkers {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			matchLinkWorker(parentCtx, jobs, results)
+		}()
+	}
+
+	for _, playerURL := range playerURLs {
+		jobs <- playerURL
+	}
+	close(jobs)
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	var matchLinks []string
+	for link := range results {
+		matchLinks = append(matchLinks, link...)
+	}
+
+	for _, link := range matchLinks {
+		log.Println(link)
+	}
+	// figure out wtf to return
+	return nil
+}
+
+func matchLinkWorker(ctx context.Context, jobs <-chan string, results chan<- []string) {
+	for matchURL := range jobs {
+		tabCtx, cancel := chromedp.NewContext(ctx)
+
+		var links []string
+		err := chromedp.Run(tabCtx,
+			chromedp.Navigate(matchURL),
+			chromedp.Sleep(5*time.Second),
+			chromedp.Evaluate(`
+				Array.from(document.querySelectorAll('a.ng-star-inserted[href^="/app/match-details/"]'))
+					.slice(0, 5)
+					.map(a => a.href)
+			`, &links),
+		)
+		cancel()
+		if err != nil {
+			log.Println("Error: ", err)
+			continue
+		}
+		results <- links
+	}
 }
 
 func (s *Server) getTopPlayers(ctx context.Context, client *http.Client, region string, limit int) (*Players, error) {
