af354ddde2237120348aa2ca7eb99afbe6ca15d3
zachar7 <springerzman@gmail.com>
2025-07-19 02:20:54 -0700

    concurrently scrape matches into go struct

--- internal/server/fetch_scrape.go
+++ internal/server/fetch_scrape.go
@@ -37,6 +37,26 @@ type PlayerDetails struct {
 	FaceitURL string `json:"faceit_url"`
 }
 
+type PlayerStats struct {
+	Name                string
+	LeetifyRating       string
+	PersonalPerformance string
+	HLTVRating          string
+	KD                  string
+	ADR                 string
+	Aim                 string
+	Utility             string
+}
+
+type Team struct {
+	Players []PlayerStats
+	Won     bool
+}
+
+type Match struct {
+	Teams [2]Team // [0]: winner, [1]: loser
+}
+
 const topPlayersURL string = "https://open.faceit.com/data/v4/rankings/games/cs2/regions/"
 const playerDetailsURL string = "https://open.faceit.com/data/v4/players/"
 const leetifyUserURL string = "https://leetify.com/app/profile/"
@@ -95,81 +115,53 @@ func (s *Server) FetchAndScrape() error {
 		leetifyURLs = append(leetifyURLs, url)
 	}
 
-	// concurrent scraping
 	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), chromedp.DefaultExecAllocatorOptions[:]...)
 	defer cancel()
 	scrapeCtx, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
+
+	// concurrent scraping of match URLs
+	// NOTE: Fix error handling, currently returns no errors
+	log.Println("Starting to scrape user profiles for matches...")
 	matchLinks, err := s.scrapeMatchLinksWithWorkers(scrapeCtx, leetifyURLs)
 	if err != nil {
 		return err
 	}
-	fmt.Println(matchLinks)
+	fmt.Println("Matches found:", len(matchLinks))
+
+	// concurrent scraping of match tables from URLs
+	// NOTE: Fix error handling, currently returns no errors
+	log.Println("Starting to scrape matches for stats...")
+	matches, err := s.scrapeMatchesWithWorkers(scrapeCtx, matchLinks)
+	if err != nil {
+		return err
+	}
+
+	for _, match := range matches {
+		fmt.Println()
+		fmt.Println("Matches: ")
+		fmt.Println("Winning team:")
+		fmt.Println(match.Teams[0])
+		fmt.Println()
+		fmt.Println("Losing team:")
+		fmt.Println(match.Teams[1])
+		fmt.Println()
+
+	}
 
 	return nil
 }
 
-func (s *Server) scrapeLeetify(players []PlayerDetails) error {
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
-	ctx, cancel = context.WithTimeout(ctx, 120*time.Second)
-	defer cancel()
-
-	log.Println("Gathering match links from Leetify")
-	var matchLinks []string
-	for _, player := range players {
-		url := leetifyUserURL + player.SteamID64
-
-		var playerLinks []string
-		err := chromedp.Run(ctx,
-			chromedp.Navigate(url),
-			chromedp.Sleep(10*time.Second),
-			chromedp.Evaluate(`
-				Array.from(document.querySelectorAll('a.ng-star-inserted[href^="/app/match-details/"]'))
-					.slice(0, 5)
-					.map(a => a.href)
-			`, &playerLinks),
-		)
-		if err != nil {
-			return fmt.Errorf("error: leetify user page: %s", err)
-		}
-		matchLinks = append(matchLinks, playerLinks...)
-		log.Println("gathered: ", len(matchLinks), " matches so far")
-	}
-
-	log.Println("finished gathering matches, scraping matches")
-	var tableData [][]string
-	for _, matchURL := range matchLinks {
-		err := chromedp.Run(ctx,
-			chromedp.Navigate(matchURL),
-			chromedp.WaitVisible(`table`, chromedp.ByQuery),
-			chromedp.Evaluate(`
-				Array.from(document.querySelectorAll('table tr'))
-					.map(row => Array.from(row.querySelectorAll('td'))
-					.map(cell => cell.textContent.trim())
-			)
-			`, &tableData),
-		)
-		if err != nil {
-			return fmt.Errorf("error: leetify match page: %s", err)
-		}
-		for _, row := range tableData {
-			log.Println(row)
-		}
-	}
-	return nil
-}
-
-func (s *Server) scrapeMatchesWithWorkers(parentCtx context.Context, matchLinks []string) error {
+func (s *Server) scrapeMatchesWithWorkers(parentCtx context.Context, matchLinks []string) ([]Match, error) {
 	numWorkers := 5
 	jobs := make(chan string, len(matchLinks))
-	results := make(chan [][]string, len(matchLinks))
+	results := make(chan [][]string, len(matchLinks)*10)
 
 	var wg sync.WaitGroup
 	for range numWorkers {
 		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			matchesWorker(parentCtx, jobs, results)
 		}()
 	}
@@ -184,15 +176,84 @@ func (s *Server) scrapeMatchesWithWorkers(parentCtx context.Context, matchLinks 
 		close(results)
 	}()
 
-	var tableData [][]string
-	for rows := range results {
-		tableData = append(tableData, rows...)
+	var allMatches [][][]string
+	for matchRows := range results {
+		allMatches = append(allMatches, matchRows)
 	}
-	return nil
+
+	var matches []Match
+	for _, matchRow := range allMatches {
+		var nonEmptyRows [][]string
+		for _, row := range matchRow {
+			if len(row) == 0 {
+				continue
+			}
+			nonEmptyRows = append(nonEmptyRows, row)
+		}
+
+		if len(nonEmptyRows) < 10 {
+			continue
+		}
+
+		var winPlayers, losePlayers []PlayerStats
+		for i, row := range nonEmptyRows[:10] {
+			p := PlayerStats{
+				Name:                row[0],
+				LeetifyRating:       row[1],
+				PersonalPerformance: row[2],
+				HLTVRating:          row[3],
+				KD:                  row[4],
+				ADR:                 row[5],
+				Aim:                 row[6],
+				Utility:             row[7],
+			}
+			if i < 5 {
+				winPlayers = append(winPlayers, p)
+			} else {
+				losePlayers = append(losePlayers, p)
+			}
+		}
+		if len(winPlayers) < 5 || len(losePlayers) < 5 {
+			log.Println("Team had less than 5 players")
+			continue
+		}
+		winTeam := Team{
+			Players: winPlayers,
+			Won:     true,
+		}
+		loseTeam := Team{
+			Players: losePlayers,
+			Won:     false,
+		}
+		match := Match{
+			Teams: [2]Team{winTeam, loseTeam},
+		}
+		matches = append(matches, match)
+	}
+	return matches, nil
 }
 
 func matchesWorker(ctx context.Context, jobs <-chan string, results chan<- [][]string) {
-
+	for matchLink := range jobs {
+		tabCtx, cancel := chromedp.NewContext(ctx)
+		var matchData [][]string
+		err := chromedp.Run(tabCtx,
+			chromedp.Navigate(matchLink),
+			chromedp.WaitVisible(`table`, chromedp.ByQuery),
+			chromedp.Evaluate(`
+				Array.from(document.querySelectorAll('table tr'))
+					.map(row => Array.from(row.querySelectorAll('td'))
+					.map(cell => cell.textContent.trim())
+			)
+			`, &matchData),
+		)
+		cancel()
+		if err != nil {
+			log.Println("Error: ", err)
+			continue
+		}
+		results <- matchData
+	}
 }
 
 func (s *Server) scrapeMatchLinksWithWorkers(parentCtx context.Context, playerURLs []string) ([]string, error) {
